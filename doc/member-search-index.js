3f801f374e4c903e47d9d7456eafc7e68e1f13c1
Xavi <xcaules20718@iesjoanramis.org>
2023-11-10 10:52:57 +0100

    Documentacio de javadoc

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"gitrepas2","c":"Empleat","l":"Empleat(String, String, double)","u":"%3Cinit%3E(java.lang.String,java.lang.String,double)"},{"p":"gitrepas2","c":"Empleat","l":"pujadaSou(double)"}];updateSearchResults();
